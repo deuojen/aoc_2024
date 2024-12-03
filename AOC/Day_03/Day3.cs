9fbcfbe5ee692c315ce11b8a62eff99c00a002ab
deuojen <qnurix@hotmail.com>
2024-12-03 09:05:38 +0000

    day 3 complete

---
+++ AOC/Day_03/Day3.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC.Day_03
+{
+    public class Day3
+    {
+        private string FilePath = "./Day_03/Input.txt";
+        public int SolutionPart1()
+        {
+            var lines = File.ReadAllLines(FilePath);
+            var total = 0;
+
+            foreach (var line in lines)
+            {
+                var regex = new Regex(@"mul\([0-9]+,[0-9]+\)");
+                var match = regex.Matches(line);
+
+                foreach (var item in match) {
+                    total += Calculate(item.ToString());
+                }
+
+            }
+
+            return total;
+        }
+
+        public int SolutionPart2()
+        {
+            var lines = File.ReadAllLines(FilePath);
+            var total = 0;
+
+            var isLastDont = false;
+
+            foreach (var line in lines)
+            {
+                var alteredLine = line;
+                if(isLastDont)
+                {
+                    alteredLine = "don't()" + alteredLine;
+                }
+
+                var cleanedLines = alteredLine.Split("do()", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var newLine in cleanedLines)
+                {
+                    var process = "";
+
+                    if(newLine.Contains("don't()"))
+                    {
+                        process = newLine.Substring(0, newLine.IndexOf("don't()"));
+                    }
+                    else
+                    {
+                        process = newLine;
+                    }
+
+                    var regex = new Regex(@"mul\([0-9]+,[0-9]+\)");
+                    var match = regex.Matches(process);
+
+                    foreach (var item in match)
+                    {
+                        total += Calculate(item.ToString());
+                    }
+                }
+
+                if (cleanedLines[cleanedLines.Length - 1].Contains("don't()"))
+                {
+                    isLastDont = true;
+                }
+                else
+                {
+                    isLastDont = false;
+                }
+            }
+
+            return total;
+        }
+
+        private int Calculate(string problem)
+        {
+            var cleanProblem = problem.Replace("mul(", "").Replace(")", "");
+
+            var splitted = cleanProblem.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            return Convert.ToInt32(splitted[0]) * Convert.ToInt32(splitted[1]);
+        }
+    }
+}
--- AOC/Program.cs
+++ AOC/Program.cs
@@ -3,12 +3,18 @@
 
 using AOC.Day_01;
 using AOC.Day_02;
+using AOC.Day_03;
 
 var day1 = new Day1();
-Console.WriteLine(day1.SolutionPart1());
-Console.WriteLine(day1.SolutionPart2());
+Console.WriteLine("Day 1 part 1 = {0}", day1.SolutionPart1());
+Console.WriteLine("Day 1 part 2 = {0}", day1.SolutionPart2());
 //Console.ReadLine();
 
 var day2 = new Day2();
-Console.WriteLine(day2.SolutionPart1());
-Console.WriteLine(day2.SolutionPart2());
+Console.WriteLine("Day 2 part 1 = {0}", day2.SolutionPart1());
+Console.WriteLine("Day 2 part 2 = {0}", day2.SolutionPart2());
+
+
+var day3 = new Day3();
+Console.WriteLine("Day 3 part 1 = {0}", day3.SolutionPart1());
+Console.WriteLine("Day 3 part 2 = {0}", day3.SolutionPart2());
